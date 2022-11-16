40f35819424366107bc3f3e4c45e16fa72a94d5f
Michelle McCann <105986556+Michelle-io@users.noreply.github.com>
2022-11-16 07:07:59 +0000

    set up structure of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+function runGame() {}
 
+function checkAnswer() {}
+
+function calcuateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function IncrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
